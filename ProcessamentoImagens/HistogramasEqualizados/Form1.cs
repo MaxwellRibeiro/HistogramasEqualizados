0a7e2156baa45dd0a04d3f91f70d35fef02658fc
Maxwell da Mata Ribeiro <maxwellmribeiro@gmail.com>
2018-09-03 19:08:21 -0300

    Materal Skin

--- ProcessamentoImagens/HistogramasEqualizados/Form1.cs
+++ ProcessamentoImagens/HistogramasEqualizados/Form1.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MaterialSkin.Controls;
 
 namespace HistogramasEqualizados
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
