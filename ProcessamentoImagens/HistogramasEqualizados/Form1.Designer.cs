9fc766f9a92a96367637c3058519efb6b9e2a18d
Maxwell da Mata Ribeiro <maxwellmribeiro@gmail.com>
2018-09-09 15:35:01 -0300

    Codificando...

--- ProcessamentoImagens/HistogramasEqualizados/Form1.Designer.cs
+++ ProcessamentoImagens/HistogramasEqualizados/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AbrirImagem = new System.Windows.Forms.OpenFileDialog();
+            this.pbImagemOriginal = new System.Windows.Forms.PictureBox();
+            this.btCarregarImagem = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btEqualizarImagem = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.pbImagemEqualizada = new System.Windows.Forms.PictureBox();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btCancelar = new MaterialSkin.Controls.MaterialRaisedButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagemOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagemEqualizada)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AbrirImagem
+            // 
+            this.AbrirImagem.FileName = "openFileDialog1";
+            // 
+            // pbImagemOriginal
+            // 
+            this.pbImagemOriginal.Location = new System.Drawing.Point(12, 106);
+            this.pbImagemOriginal.Name = "pbImagemOriginal";
+            this.pbImagemOriginal.Size = new System.Drawing.Size(186, 131);
+            this.pbImagemOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagemOriginal.TabIndex = 0;
+            this.pbImagemOriginal.TabStop = false;
+            // 
+            // btCarregarImagem
+            // 
+            this.btCarregarImagem.Depth = 0;
+            this.btCarregarImagem.Location = new System.Drawing.Point(12, 243);
+            this.btCarregarImagem.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btCarregarImagem.Name = "btCarregarImagem";
+            this.btCarregarImagem.Primary = true;
+            this.btCarregarImagem.Size = new System.Drawing.Size(186, 33);
+            this.btCarregarImagem.TabIndex = 1;
+            this.btCarregarImagem.Text = "Carregar Imagem";
+            this.btCarregarImagem.UseVisualStyleBackColor = true;
+            this.btCarregarImagem.Click += new System.EventHandler(this.btCarregarImagem_Click);
+            // 
+            // btEqualizarImagem
+            // 
+            this.btEqualizarImagem.Depth = 0;
+            this.btEqualizarImagem.Location = new System.Drawing.Point(12, 419);
+            this.btEqualizarImagem.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btEqualizarImagem.Name = "btEqualizarImagem";
+            this.btEqualizarImagem.Primary = true;
+            this.btEqualizarImagem.Size = new System.Drawing.Size(186, 33);
+            this.btEqualizarImagem.TabIndex = 3;
+            this.btEqualizarImagem.Text = "Equalizar Imagem";
+            this.btEqualizarImagem.UseVisualStyleBackColor = true;
+            this.btEqualizarImagem.Click += new System.EventHandler(this.btEqualizarImagem_Click);
+            // 
+            // pbImagemEqualizada
+            // 
+            this.pbImagemEqualizada.Location = new System.Drawing.Point(12, 282);
+            this.pbImagemEqualizada.Name = "pbImagemEqualizada";
+            this.pbImagemEqualizada.Size = new System.Drawing.Size(186, 131);
+            this.pbImagemEqualizada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagemEqualizada.TabIndex = 2;
+            this.pbImagemEqualizada.TabStop = false;
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProgressBar.Location = new System.Drawing.Point(0, 64);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(1314, 23);
+            this.ProgressBar.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Depth = 0;
+            this.btCancelar.Location = new System.Drawing.Point(12, 489);
+            this.btCancelar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Primary = true;
+            this.btCancelar.Size = new System.Drawing.Size(186, 33);
+            this.btCancelar.TabIndex = 5;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1314, 702);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.btEqualizarImagem);
+            this.Controls.Add(this.pbImagemEqualizada);
+            this.Controls.Add(this.btCarregarImagem);
+            this.Controls.Add(this.pbImagemOriginal);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Processamento de Imagens : Histograma e Equalização";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagemOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagemEqualizada)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog AbrirImagem;
+        private System.Windows.Forms.PictureBox pbImagemOriginal;
+        private MaterialSkin.Controls.MaterialRaisedButton btCarregarImagem;
+        private MaterialSkin.Controls.MaterialRaisedButton btEqualizarImagem;
+        private System.Windows.Forms.PictureBox pbImagemEqualizada;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private MaterialSkin.Controls.MaterialRaisedButton btCancelar;
     }
 }
 
--- ProcessamentoImagens/HistogramasEqualizados/Form1.cs
+++ ProcessamentoImagens/HistogramasEqualizados/Form1.cs
@@ -1,12 +1,115 @@
 ﻿using MaterialSkin.Controls;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace HistogramasEqualizados
 {
     public partial class Form1 : MaterialForm
     {
+        private Bitmap _imagem;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btCarregarImagem_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog File1 = new OpenFileDialog();
+            File1.Filter = "JPEG files (*.jpg)|*.jpg";
+            if (File1.ShowDialog() == DialogResult.OK)
+            {
+                if (_imagem != null)
+                {
+                    _imagem.Dispose();
+                }
+                _imagem = new Bitmap(File1.FileName);
+                pbImagemOriginal.Image = _imagem;
+            }
+        }
+
+        private void EqualizarImagem(System.ComponentModel.DoWorkEventArgs e)
+        {
+            Color pixel1;
+            int r, g, b;
+            for (int x = 0; x < _imagem.Width; x++)
+                for (int y = 0; y < _imagem.Height; y++)
+                {
+                    VerificarCancelamento(e);
+
+                    pixel1 = _imagem.GetPixel(x, y);
+                    r = pixel1.R + 100;
+                    g = pixel1.G + 100;
+                    b = pixel1.B + 100;
+                    _imagem.SetPixel(x, y, Color.FromArgb(r > 255 ? 255 : r, g > 255 ? 255 : g, b > 255 ? 255 : b));
+                }
+            pbImagemEqualizada.Image = _imagem;
+
+        }
+
+        private void VerificarCancelamento(System.ComponentModel.DoWorkEventArgs e)
+        {
+            //Verifica se houve uma requisição para cancelar a operação.
+            if (backgroundWorker1.CancellationPending)
+            {
+                //se sim, define a propriedade Cancel para true
+                //para que o evento WorkerCompleted saiba que a tarefa foi cancelada.
+                e.Cancel = true;
+                return;
+            }
+        }
+
+        private void btEqualizarImagem_Click(object sender, System.EventArgs e)
+        {
+            //desabilita os botões enquanto a tarefa é executada.
+            backgroundWorker1.RunWorkerAsync();
+
+            //define a progressBar para Marquee
+            ProgressBar.Style = ProgressBarStyle.Marquee;
+            ProgressBar.MarqueeAnimationSpeed = 5;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            EqualizarImagem(e);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            //Caso cancelado...
+            if (e.Cancelled)
+            {
+                // reconfigura a progressbar para o padrao.
+                ProgressBar.MarqueeAnimationSpeed = 0;
+                ProgressBar.Style = ProgressBarStyle.Blocks;
+                ProgressBar.Value = 0;
+                
+            }
+            else if (e.Error != null)
+            {
+                // reconfigura a progressbar para o padrao.
+                ProgressBar.MarqueeAnimationSpeed = 0;
+                ProgressBar.Style = ProgressBarStyle.Blocks;
+                ProgressBar.Value = 0;
+            }
+            else
+            {
+                //Carrega todo progressbar.
+                ProgressBar.MarqueeAnimationSpeed = 0;
+                ProgressBar.Style = ProgressBarStyle.Blocks;
+                ProgressBar.Value = 100;
+            }
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+            {
+                // notifica a thread que o cancelamento foi solicitado.
+                // Cancela a tarefa DoWork 
+                backgroundWorker1.CancelAsync();
+            }
         }
     }
 }
