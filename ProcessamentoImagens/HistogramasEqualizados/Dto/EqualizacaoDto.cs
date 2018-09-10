c695ae234249fe3122e5e044ed3c1698ef39cd0d
Maxwell da Mata Ribeiro <maxwellmribeiro@gmail.com>
2018-09-09 23:57:52 -0300

    ....

--- ProcessamentoImagens/HistogramasEqualizados/Dto/EqualizacaoDto.cs
+++ ProcessamentoImagens/HistogramasEqualizados/Dto/EqualizacaoDto.cs
@@ -3,7 +3,6 @@
     public class EqualizacaoDto
     {
         public int K { get; set; }
-        public int NK { get; set; }
         public decimal S { get; set; }
         public int NEWK { get; set; }
     }
